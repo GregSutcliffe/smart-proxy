58537eb976afe1c7b55986fe9cdc2268c3ad58ad
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2014-04-04 15:46:21 +0100

    support for http + https modules

--- bin/smart-proxy-new.rb
+++ bin/smart-proxy-new.rb
@@ -4,48 +4,9 @@ $LOAD_PATH.unshift *Dir[File.expand_path("../../lib", __FILE__), File.expand_pat
 
 require 'bundler'
 require 'json'
-
 require 'smart_proxy_new'
 
 Bundler.require
 
-if SETTINGS.daemon
-  pid_path = File.dirname(SETTINGS.daemon_pid)
-  FileUtils.mkdir_p(pid_path) unless File.exists?(pid_path)
-end
+Proxy::Launcher.launch
 
-app = Rack::Builder.new do
-  configs = ObjectSpace.each_object(::Class).select {|klass| klass < ::Proxy::ModuleConfig}
-  configs.each {|c| instance_eval(File.read(c.http_rackup_path))}
-end
-
-#app1 = Rack::Builder.new do
-#  configs = ObjectSpace.each_object(::Class).select {|klass| klass < ::Proxy::ModuleConfig}
-#  configs.each {|c| instance_eval(File.read(c.rackup_config_path))}
-#end
-
-s1 = Rack::Server.new(
-  :app => app, 
-  :server => :webrick,
-  :Port => SETTINGS.port,
-  :daemonize => SETTINGS.daemon,
-  :pid => SETTINGS.daemon ? pid_path : nil)
-
-#s2 =   Rack::Server.new(
-#    :app => app1, 
-#    :server => :webrick,
-#    :Port => 1235,
-#    :daemonize => false,
-#    :pid => nil)
-
-t1 = Thread.new { s1.start }  
-#t2 = Thread.new { s2.start }
-
-#trap(:INT) do
-#  s1.shutdown
-#  s2.shutdown
-#end
-
-t1.join
-
-Process.daemon
--- lib/smart_proxy_new.rb
+++ lib/smart_proxy_new.rb
@@ -36,4 +36,96 @@ module Proxy
   def self.features
     MODULES.collect{|mod| mod if SETTINGS.send mod}.compact
   end
+
+  class Launcher
+    include ::Proxy::Log
+
+    def pid_path
+      SETTINGS.daemon_pid
+    end
+
+    def create_pid_dir
+      if SETTINGS.daemon
+        FileUtils.mkdir_p(File.dirname(pid_path)) unless File.exists?(pid_path)
+      end
+    end
+
+    def http_app
+      app = Rack::Builder.new do
+        configs = ObjectSpace.each_object(::Class).select {|klass| klass < ::Proxy::ModuleConfig}
+        configs.each {|c| instance_eval(File.read(c.http_rackup_path))}
+      end
+
+      Rack::Server.new(
+        :app => app, 
+        :server => :webrick,
+        :Port => SETTINGS.http_port,
+        :daemonize => false,
+        :pid => SETTINGS.daemon ? pid_path : nil)
+    end
+
+    def http_app_2
+      return nil
+      
+      app = Rack::Builder.new do
+        configs = ObjectSpace.each_object(::Class).select {|klass| klass < ::Proxy::ModuleConfig}
+        configs.each {|c| instance_eval(File.read(c.http_rackup_path))}
+      end
+
+      Rack::Server.new(
+        :app => app, 
+        :server => :webrick,
+        :Port => SETTINGS.https_port,
+        :daemonize => false,
+        :pid => SETTINGS.daemon ? pid_path : nil)
+    end
+
+    def https_app
+      unless SETTINGS.ssl_private_key and SETTINGS.ssl_certificate and SETTINGS.ssl_ca_file
+        logger.warn "WARNING: Missing SSL setup, working in clear text mode !\n"
+      else
+        begin
+          app = Rack::Builder.new do
+            configs = ObjectSpace.each_object(::Class).select {|klass| klass < ::Proxy::ModuleConfig}
+            configs.each {|c| instance_eval(File.read(c.https_rackup_path))}
+          end
+
+          Rack::Server.new(
+            :app => app,
+            :server => :webrick,
+            :Port => SETTINGS.https_port,
+            :SSLEnable => true,
+            :SSLVerifyClient => OpenSSL::SSL::VERIFY_PEER,
+            :SSLPrivateKey => OpenSSL::PKey::RSA.new(File.read(SETTINGS.ssl_private_key)),
+            :SSLCertificate => OpenSSL::X509::Certificate.new(File.read(SETTINGS.ssl_certificate)),
+            :SSLCACertificateFile => SETTINGS.ssl_ca_file,
+            :daemonize => false,
+            :pid => nil)
+        rescue => e
+          logger.warn "Unable to access the SSL keys. Are the values correct in settings.yml and do permissions allow reading?: #{e}"
+        end
+      end
+
+    end
+
+    def self.launch
+      launcher = Launcher.new
+
+      launcher.create_pid_dir
+      http_app = launcher.http_app
+      https_app = launcher.https_app
+
+      t1 = Thread.new { http_app.start }  
+      t2 = Thread.new { https_app.start } unless https_app.nil?
+
+      trap(:INT) do
+        http_app.shutdown
+        https_app.shutdown unless https_app.nil?
+      end
+
+      t1.join
+
+      Process.daemon if SETTINGS.daemon
+    end
+  end
 end
