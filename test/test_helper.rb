d61a7eb97a22f575a5b1a88732eb4191b570de11
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2014-04-24 19:12:45 +0100

    no need for testing_settings file anymore

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ logdir = File.join(File.dirname(__FILE__), '..', 'logs')
 FileUtils.mkdir_p(logdir) unless File.exists?(logdir)
 
 require 'proxy/proxy'
-require 'testing_settings'
+require 'proxy/settings'
 require 'proxy/log'
 require 'proxy/util'
 require 'proxy/helpers'
@@ -14,3 +14,7 @@ require 'proxy/plugin'
 
 require "mocha/setup"
 require "rack/test"
+
+Proxy::SETTINGS_PATH = File.expand_path("fixtures/test_settings.yml", File.dirname(__FILE__))
+Proxy::SETTINGS = Settings.load_from_file(:settings_path => Proxy::SETTINGS_PATH)
+
--- test/testing_settings.rb
+++
@@ -1,3 +0,0 @@
-require "proxy/settings"
-Proxy::SETTINGS_PATH = File.expand_path("fixtures/test_settings.yml", File.dirname(__FILE__))
-Proxy::SETTINGS = Settings.load_from_file(:settings_path => Proxy::SETTINGS_PATH)
