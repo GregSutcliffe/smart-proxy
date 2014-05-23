b2de4be4bb05988f39253ef3f109274185905eac
Greg Sutcliffe <gsutclif@redhat.com>
2014-05-23 12:48:28 +0100

    Fix another instance of SETTINGS

--- modules/dhcp/dhcp/providers/server/isc.rb
+++ modules/dhcp/dhcp/providers/server/isc.rb
@@ -92,7 +92,7 @@ module Proxy::DHCP
       super
       @config.each_line do |line|
         if line =~ /^\s*subnet\s+([\d\.]+)\s+netmask\s+([\d\.]+)/
-          next if (managed_subnets = SETTINGS.dhcp_subnets) and !managed_subnets.include? "#{$1}/#{$2}"
+          next if (managed_subnets = Proxy::DhcpPlugin.settings.dhcp_subnets) and !managed_subnets.include? "#{$1}/#{$2}"
 
           Proxy::DHCP::Subnet.new(self, $1, $2)
         end
